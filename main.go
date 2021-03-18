f032ed2067d73139f91de4e8a8d8b5e3b980eb54
Tony Natale <anatale@protonmail.com>
2021-03-18 09:10:21 -0400

    initial commit

--- main.go
+++ main.go
@@ -4,13 +4,29 @@ import (
 	"canitconnect/web"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("web/templates/*.html"))
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("web/assets")))
+	http.HandleFunc("/", serveIndex)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/api/v1/curl", web.CallCurl())
 	http.Handle("/api/v1/traceroute", web.CallTraceroute())
 	http.Handle("/api/v1/netcat", web.CallNetcat())
+	http.Handle("/curl", web.CallCurl())
+	http.Handle("/traceroute", web.CallTraceroute())
+	http.Handle("/netcat", web.CallNetcat())
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
